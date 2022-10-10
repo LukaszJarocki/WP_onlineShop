034d07f8b8e69c0d9d8756fcf696ffc8ac098595
LukaszJarocki <93542313+LukaszJarocki@users.noreply.github.com>
2022-10-10 09:59:17 +0200

    udpdate prrimary menu

--- front-page.php
+++ front-page.php
@@ -53,7 +53,7 @@ get_header();
 					We offer a number of hight quality toys to halp keep <br> your pets healthy and spoiled!
 				</p>
 				<div class="pt-5 pb-5">
-					<?php echo do_shortcode( '[products columns=4 limit=4]'); ?>
+					<?php echo do_shortcode( '[products popularity columns=4 limit=4]'); ?>
 				</div>
 		</section>
 
@@ -107,8 +107,12 @@ get_header();
 							</a>
 					</div>
 				</div>
+
+
+
 			</div>
 		</section>
+
 
 		<section class="container pt-5">
 				<h1 class="text-center pt-5">Special Offers</h1>
